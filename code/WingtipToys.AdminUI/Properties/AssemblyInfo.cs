6a847d0a24a20eef39f7e78b36363c738eb89635
J. Tower <jtower@trailheadtechnology.com>
2025-06-09 14:23:06 -0400

    Renaming apps to more accurate UI names

--- code/WingtipToys.AdminUI/Properties/AssemblyInfo.cs
+++ code/WingtipToys.AdminUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WingtipToys.MVC")]
+[assembly: AssemblyTitle("WingtipToys.AdminUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WingtipToys.MVC")]
+[assembly: AssemblyProduct("WingtipToys.AdminUI")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
